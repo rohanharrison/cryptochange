96caf8b7113f1fa205eea8562bda605da094fcbe
Rohan Harrison <harrison.rohan@gmail.com>
2017-12-10 18:35:52 -0800

    Project Init

---
+++ cryptochange/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  cryptochange
+//
+//  Created by Rohan Harrison on 12/10/17.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
