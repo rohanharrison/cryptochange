00dc9d2bfba019f34ddcc4f764d262361791b888
Rohan Harrison <harrison.rohan@gmail.com>
2017-12-10 19:18:00 -0800

    Playing with Buttons

--- cryptochange/FirstViewController.swift
+++ cryptochange/FirstViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBAction func testPrint(_ sender: Any) {
+        print("meow")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
